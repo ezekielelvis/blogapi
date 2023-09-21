5587a1c6d2fa38fd0e3d36f890c6602f1dc44ea0
elvis <ezeamartey777@gmail.com>
2023-09-21 23:09:26 +0000

    post request

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas,models
 from .database import engine,SessionLocal
 from sqlalchemy.orm import Session
@@ -15,10 +15,7 @@ def get_db():
     finally:
         db.close()
 
-
-
-
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -30,3 +27,35 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs  
+
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+def show(id,response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    return blog
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": "Blog not found"}
+    return blog
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    db.delete(blog)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/blog/{id}", status_code=status.HTTP_200_OK)
+def update(id,request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=404, detail="Blog not found")
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    return blog
+
+
+
